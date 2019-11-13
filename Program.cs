fd4083496e5dde10a14a6ae63f0cd01770346cf3
Jose Vargas <jovar2407@gmail.com>
2019-11-13 13:58:55 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
